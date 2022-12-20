cdd9880b4feebc25a5f3de837a8fb05b0230ca31
micheleciociola <micheleciociola95@live.it>
2022-12-20 11:21:35 -0800

    Second day completed

--- src/DayChallenges/DayChallenge2.php
+++ src/DayChallenges/DayChallenge2.php
@@ -17,6 +17,9 @@ class DayChallenge2 extends DayChallengeBase
     private const DEFEAT = 'DEFEAT';
     private const WIN = 'WIN';
     private const DRAW = 'DRAW';
+    private const DEFEAT_PT2 = 'X';
+    private const DRAW_PT2 = 'Y';
+    private const WIN_PT2 = 'Z';
     protected $title = 'Day Two';
     protected $legends_pt1 = [
         self::MYROCK    => 1,
@@ -26,7 +29,23 @@ class DayChallenge2 extends DayChallengeBase
         self::DRAW      => 3,
         self::WIN       => 6,
     ];
-
+    protected $outcome_legend = [
+        self::ROCK => [
+            'towin' => self::MYPAPER,
+            'tolose' => self::MYSCISSOR,
+            'todraw' => self::MYROCK,
+        ],
+        self::PAPER => [
+            'towin' => self::MYSCISSOR,
+            'tolose' => self::MYROCK,
+            'todraw' => self::MYPAPER,
+        ],
+        self::SCISSOR => [
+            'towin' => self::MYROCK,
+            'tolose' => self::MYPAPER,
+            'todraw' => self::MYSCISSOR,
+        ],
+    ];
     public function printFirstPartSolution(): void
     {
         PrintCmnFns::printSubtitle($this->title . ' PT1');
@@ -60,6 +79,35 @@ class DayChallenge2 extends DayChallengeBase
 
     public function printSecondPartSolution(): void
     {
+        PrintCmnFns::printSubtitle($this->title . ' PT2');
+        $fp = fopen('csv/day2/strategy.csv', 'r');
+        $my_total_score = 0;
+        $total_win = 0;
+        $total_draw = 0;
+        $total_defeat = 0;
+        $total_battles = 0;
+        while ($row = fgetcsv($fp)) {
+            list($response, $outcome) = explode(' ', $row[0]);
+            // PrintCmnFns::printSimpleRow($outcome);
+            $myresponse = $this->getResponseToGive($response, $outcome);
+            $score = $this->legends_pt1[$myresponse];
+
+            if ($this->isWin($response, $myresponse)) {
+                $total_win++;
+                $score += 6;
+            } elseif ($this->isDraw($response, $myresponse)) {
+                $total_draw++;
+                $score += 3;
+            } else {
+                $total_defeat++;
+            }
+            $my_total_score += $score;
+            $total_battles++;
+        }
+        PrintCmnFns::printRow('Il numero delle mie vittorie è: ', "$total_win/$total_battles");
+        PrintCmnFns::printRow('Il numero delle mie sconfitte è: ', "$total_defeat/$total_battles");
+        PrintCmnFns::printRow('Il numero dei miei paregggi è: ', "$total_draw/$total_battles");
+        PrintCmnFns::printRow('Il mio punteggio totale è:', $my_total_score);
     }
 
     private function isWin(string $response, string $myresponse): bool
@@ -81,5 +129,22 @@ class DayChallenge2 extends DayChallengeBase
     private function getOutcome($response, $myresponse): string
     {
         return $this->isWin($response, $myresponse) ? self::WIN : ($this->isDraw($response, $myresponse) ? self::DRAW : self::DEFEAT);
+    }
+    
+    private function getResponseToGive(string $response, string $outcome) {
+        switch ($outcome) {
+            case self::DEFEAT_PT2:
+                $myresponse = $this->outcome_legend[$response]['tolose'];
+                break;
+            case self::DRAW_PT2:
+                $myresponse = $this->outcome_legend[$response]['todraw'];
+                break;
+            case self::WIN_PT2:
+                $myresponse = $this->outcome_legend[$response]['towin'];
+                break;
+            default:
+                throw new Exception('Outcome Not Managed');
+        }
+        return $myresponse;
     }
 }
